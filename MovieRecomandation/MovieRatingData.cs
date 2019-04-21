7aeac34bf3cb1b9d73a8e9174d919cc6fd0bfc1c
Layth CHEBBI <layth-chebbi@outlook.com>
2019-04-21 15:06:30 +0100

    Add project files.

---
+++ MovieRecomandation/MovieRatingData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ML.Data;
+
+namespace MovieRecomandation
+{
+    class MovieRatingData
+    {
+    }
+
+    public class MovieRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float movieId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
---
+++ MovieRecomandation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Data;
+using Microsoft.Data.DataView;
+using Microsoft.ML;
+
+namespace MovieRecomandation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mLContext = new MLContext();
+            IDataView trainingDataView = LoadData(mLContext).training;
+            IDataView testDataView = LoadData(mLContext).test;
+
+        }
+
+        public static(IDataView training,IDataView test) LoadData(MLContext mLContext)
+        {
+            var trainingDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-train.csv");
+            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-test.csv");
+
+            IDataView trainingDataView = mLContext.Data.LoadFromTextFile<MovieRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+            IDataView testDataView = mLContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
+
+            return (trainingDataView, testDataView);
+        }
+    }
+}
